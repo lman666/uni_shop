3ca5010252d0e30ecab1e9fbc4b5e51849cbb30e
lman666 <1720889474@qq.com>
2022-08-19 22:01:01 +0800

    完成了商品列表页面的开发

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { $http } from '@escook/request-miniprogram'
 
 uni.$http = $http
 // 请求根路径
-$http.baseUrl = 'https://api-ugo-web.itheima.net'
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
 
 // 请求拦截器
 $http.beforeRequest = function(options) {
