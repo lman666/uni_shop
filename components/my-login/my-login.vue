44569fa7e016928f8cb0fc935b674e6b79dd4f72
lman666 <1720889474@qq.com>
2022-08-22 16:49:46 +0800

    完成了登录和支付的功能

---
+++ components/my-login/my-login.vue
@@ -0,0 +1,107 @@
+<template>
+  <view class="login-container">
+    <uni-icons type="contact-filled" size="100" color="#afafaf"></uni-icons>
+    <button type="primary" class="btn-login" @click="getUserProfile">一键登录</button>
+    <text class="tips-text">登录后尽享更多权益</text>
+  </view>
+</template>
+
+<script>
+  import {mapMutations, mapState} from 'vuex'
+  
+  export default {
+    data() {
+      return {
+
+      };
+    },
+    computed: {
+      ...mapState('m_user', ['redirectInfo'])
+    },
+    methods: {
+      ...mapMutations('m_user', ['updateUserInfo', 'updateToken', 'updateRedirectInfo']),
+      // 用户授权之后，获取用户的基本信息
+      getUserProfile(e) {
+        uni.getUserProfile({
+          desc: '用于完善会员资料',
+          success: (res) => {
+            this.updateUserInfo(res.userInfo)
+            this.getToken(res)
+          },
+          fail: (res) => {
+            return uni.$showMsg('您取消了登录授权')
+          }
+        })
+      },
+      async getToken(info) {
+        // 获取 code 对应的值
+        const [err, res] = await uni.login().catch(err => err)
+        if (err || res.errMsg !== 'login:ok') return uni.$showMsg('登陆失败！')
+        
+        // 准备参数
+        const query = {
+          code: res.code,
+          encryptedData: info.encryptedData,
+          iv: info.iv,
+          rawData: info.rawData,
+          signature: info.signature
+        }
+        const { data: loginResult } = await uni.$http.post('/api/public/v1/users/wxlogin', query)
+        // if (loginResult.meta.status !== 200) return uni.$showMsg('登陆失败！')
+        
+        // 直接把 token 保存到 vuex 中
+        // this.updateToken(loginResult.message.token)
+        this.updateToken("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo")
+        this.navigateBack()
+      },
+      navigateBack() {
+        if (this.redirectInfo && this.redirectInfo.openType === 'switchTab') {
+          uni.switchTab({
+            url: this.redirectInfo.from,
+            complete: () => {
+              this.updateRedirectInfo(null)
+            }
+          })
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .login-container {
+    height: 750rpx;
+    background-color: #f8f8f8;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    
+    &::after {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 40px;
+      background-color: white;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      border-radius: 100%;
+      transform: translateY(50%);
+    }
+    
+    .btn-login {
+      width: 90%;
+      border-radius: 100px;
+      margin: 15px 0;
+      background-color: #c00000;
+    }
+
+    .tips-text {
+      font-size: 12px;
+      color: gray;
+    }
+  }
+</style>
